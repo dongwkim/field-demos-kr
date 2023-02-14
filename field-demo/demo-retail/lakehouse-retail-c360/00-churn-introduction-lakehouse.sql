dbcde2f0092fa275590f83e84329761cc46b1b62
dongwkim <dongwkim>
2023-02-14 14:45:36 +0000

    introduction korean translate

--- field-demo/demo-retail/lakehouse-retail-c360/00-churn-introduction-lakehouse.sql
+++ field-demo/demo-retail/lakehouse-retail-c360/00-churn-introduction-lakehouse.sql
@@ -1,11 +1,12 @@
 -- Databricks notebook source
 -- MAGIC %md-sandbox
--- MAGIC # Lakehouse for retail - Reducing Churn with a Customer 360 platform
+-- MAGIC # Lakehouse for retail - Customer 360 플랫폼으로 고객 이탈 감소
 -- MAGIC 
 -- MAGIC <img src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn.png" style="float: left; margin-right: 30px" width="600px" />
 -- MAGIC 
 -- MAGIC <br/>
--- MAGIC 
+-- MAGIC <div style="padding-left: 630px">
+-- MAGIC   
 -- MAGIC ## Retail 에서의 Databricks Lakehouse는 무엇입니까?
 -- MAGIC 
 -- MAGIC 모든 워크로드에서 모든 소스의 모든 데이터를 활용하여 항상 최저 비용과 실시간 데이터로 구동되는 보다 매력적인 고객 경험을 제공할 수 있는 유일한 엔터프라이즈 데이터 플랫폼입니다.
@@ -41,9 +42,9 @@
 -- MAGIC 
 -- MAGIC 대략적으로 구현할 흐름은 다음과 같습니다.
 -- MAGIC 
--- MAGIC <img width="1000px" src="https://github.com/databricks-demos/dbdemos-resources/raw/main/images/retail/lakehouse-churn/lakehouse-retail-c360-churn-0.png" />
+-- MAGIC <!-- <img width="800px" src="https://github.com/databricks-demos/dbdemos-resources/raw/main/images/retail/lakehouse-churn/lakehouse-retail-c360-churn-0.png" /> -->
 -- MAGIC 
--- MAGIC <img width="1000px" src="/Workspace/Repos/dongwook.kim@databricks.com/field-demos-kr/field-demo/images/retail/lakehouse-chrun/lakehouse-retail-c360-churn-0.png"/>
+-- MAGIC <img width="900px" src="https://raw.githubusercontent.com/dongwkim/field-demos-kr/v0214/field-demo/images/retail/lakehouse-chrun/lakehouse-retail-c360-churn-0.png"/>
 -- MAGIC 
 -- MAGIC 1. 데이터를 수집하고 c360 데이터베이스를 생성하며, SQL에서 쉽게 쿼리할 수 있는 테이블을 사용해야 합니다.
 -- MAGIC 2. 데이터를 보호하고 데이터 분석가 및 데이터 과학 팀에 대한 읽기 액세스 권한을 부여합니다.
@@ -60,34 +61,32 @@
 -- MAGIC - 주문 내역 *(시간이 지남에 따라 고객이 구입하는 것)*
 -- MAGIC - 당사 애플리케이션의 이벤트 *(고객이 애플리케이션을 마지막으로 사용한 시기, 클릭, 일반적으로 스트리밍)*
 -- MAGIC 
--- MAGIC *기술적 수준에서 데이터는 모든 소스에서 가져올 수 있습니다. Databricks는 모든 시스템(SalesForce, Fivetran, kafka와 같은 대기열 메시지, Blob 저장소, SQL 및 NoSQL 데이터베이스...)에서 데이터를 수집할 수 있습니다.*
+-- MAGIC *기술적으로 데이터는 모든 소스에서 가져올 수 있습니다. Databricks는 모든 시스템(SalesForce, Fivetran, kafka와 같은 대기열 메시지, Blob 저장소, SQL 및 NoSQL 데이터베이스...)에서 데이터를 수집할 수 있습니다.*
 -- MAGIC 
 -- MAGIC Lakehouse 내에서 이 데이터를 사용하여 고객 이탈을 분석하고 줄이는 방법을 살펴보겠습니다!
 
 -- COMMAND ----------
 
 -- MAGIC %md-sandbox
--- MAGIC ## 1/ Ingesting and preparing the data (Data Engineering)
+-- MAGIC ## 1/ 데이터 수집 및 준비(데이터 엔지니어링)
 -- MAGIC 
 -- MAGIC <img style="float: left; margin-right: 20px" width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-2.png" />
 -- MAGIC 
 -- MAGIC 
 -- MAGIC <br/>
 -- MAGIC <div style="padding-left: 420px">
--- MAGIC Our first step is to ingest and clean the raw data we received so that our Data Analyst team can start running analysis on top of it.
+-- MAGIC 첫 번째 단계는 데이터 분석가 팀이 분석을 수행할 수 있도록 원시 데이터를 수집하고 클린징 하는 것 입니다. 
 -- MAGIC 
 -- MAGIC 
 -- MAGIC <img src="https://pages.databricks.com/rs/094-YMS-629/images/delta-lake-logo.png" style="float: right; margin-top: 20px" width="200px">
 -- MAGIC 
--- MAGIC ### Delta Lake
+-- MAGIC ### 델타 레이크 (Delta Lake)
 -- MAGIC 
--- MAGIC All the tables we'll create in the Lakehouse will be stored as Delta Lake table. [Delta Lake](https://delta.io) is an open storage framework for reliability and performance. <br/>
--- MAGIC It provides many functionalities *(ACID Transaction, DELETE/UPDATE/MERGE, Clone zero copy, Change data Capture...)* <br />
--- MAGIC For more details on Delta Lake, run `dbdemos.install('delta-lake')`
+-- MAGIC Lakehouse에서 생성할 모든 테이블은 Delta Lake 테이블로 저장됩니다. <br> [Delta Lake](https://delta.io)는 안정성과 성능을 위한 개방형 스토리지 프레임워크이며 많은 기능을 제공합니다 *(ACID Transaction, DELETE/UPDATE/MERGE, Clone zero copy, Change data Capture...)* <br />
 -- MAGIC 
--- MAGIC ### Simplify ingestion with Delta Live Tables (DLT)
+-- MAGIC ### 델타 라이브 테이블(DLT)로 수집 간소화
 -- MAGIC 
--- MAGIC Databricks simplifies data ingestion and transformation with Delta Live Tables by allowing SQL users to create advanced pipelines, in batch or streaming. The engine will simplify pipeline deployment and testing and reduce operational complexity, so that you can focus on your business transformation and ensure data quality.<br/>
+-- MAGIC Databricks는 SQL 사용자가 배치 또는 스트리밍으로 고급 파이프라인을 만들 수 있도록 허용하여 Delta Live Tables로 데이터 수집 및 변환을 간소화합니다. 이 엔진은 파이프라인 배포 및 테스트를 간소화하고 운영 복잡성을 줄여주므로 비즈니스 혁신에 집중하고 데이터 품질을 보장할 수 있습니다.<br/>
 
 -- COMMAND ----------
 
@@ -99,18 +98,18 @@
 -- COMMAND ----------
 
 -- MAGIC %md-sandbox
--- MAGIC ## 2/ Securing data & governance (Unity Catalog)
+-- MAGIC ## 2/ 데이터 보호 및 거버넌스 (Unity Catalog)
 -- MAGIC 
 -- MAGIC <img style="float: left" width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-6.png" />
 -- MAGIC 
 -- MAGIC <br/><br/><br/>
 -- MAGIC <div style="padding-left: 420px">
--- MAGIC   Now that our first tables have been created, we need to grant our Data Analyst team READ access to be able to start alayzing our Customer churn information.
+-- MAGIC   이제 첫 번째 테이블이 생성되었으므로 데이터 분석가 팀에 고객 이탈 정보 분석을 시작할 수 있도록 읽기(READ) 액세스 권한을 부여해야 합니다.
 -- MAGIC   
--- MAGIC   Let's see how Unity Catalog provides Security & governance across our data assets with, including data lineage and audit log.
+-- MAGIC    Unity Catalog가 데이터 리니지 및 감사 로그를 포함하여 데이터 자산 전체에 **보안 및 거버넌스** 를 제공하는 방법을 살펴보겠습니다.
 -- MAGIC   
--- MAGIC   Note that Unity Catalog integrates Delta Sharing, an open protocol to share your data with any external organization, regardless of their stack. For more details:  `dbdemos.install('delta-sharing-airlines')`
--- MAGIC  </div>
+-- MAGIC    Unity Catalog는 스택에 관계없이 모든 외부 조직과 데이터를 공유하는 개방형 프로토콜인 Delta Sharing을 통합합니다.
+-- MAGIC </div>
 
 -- COMMAND ----------
 
@@ -123,17 +122,17 @@
 -- MAGIC %md-sandbox
 -- MAGIC ## 3/ Analysing churn analysis  (BI / Data warehousing / SQL) 
 -- MAGIC 
--- MAGIC <img width="300px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-dbsql-dashboard.png"  style="float: right; margin: 100px 0px 10px;"/>
+-- MAGIC <img width="300px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-dbsql-dashboard.png"  style="float: right; margin: 100px 10px 10px;"/>
 -- MAGIC 
 -- MAGIC 
 -- MAGIC <img width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-3.png"  style="float: left; margin-right: 10px"/>
 -- MAGIC  
 -- MAGIC <br><br><br>
--- MAGIC Our datasets are now properly ingested, secured, with a high quality and easily discoverable within our organization.
+-- MAGIC 우리의 데이터 세트는 이제 고품질로 적절하게 수집되고 보호되며 조직 내에서 쉽게 검색할 수 있습니다.
 -- MAGIC 
--- MAGIC Data Analysts are now ready to run BI interactive queries, with low latencies & high througput, including Serverless Datawarehouses providing instant stop & start.
+-- MAGIC **데이터 분석가**는 이제 즉각적인 중지 및 시작을 제공하는 Serverless Datawarehouse를 포함하여 대기 시간이 짧고 처리량이 높은 BI 대화형 쿼리를 실행할 준비가 되었습니다.
 -- MAGIC 
--- MAGIC Let's see how we Data Warehousing can done using Databricks, including with external BI solutions like PowerBI, Tableau and other!
+-- MAGIC PowerBI, Tableau 등과 같은 외부 BI 솔루션을 포함하여 Databricks를 사용하여 데이터 웨어하우징을 수행하는 방법을 살펴보겠습니다!
 
 -- COMMAND ----------
 
@@ -148,11 +147,11 @@
 -- MAGIC <img width="400px" style="float: left; margin-right: 10px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-4.png" />
 -- MAGIC 
 -- MAGIC <br><br><br>
--- MAGIC Being able to run analysis on our past data already gave us a lot of insight to drive our business. We can better understand which customers are churning evaluate the churn impact.
+-- MAGIC 과거 데이터에 대한 분석을 실행할 수 있어 이미 비즈니스를 추진할 수 있는 많은 통찰력을 얻었습니다. 어떤 고객이 이탈하는지 더 잘 이해할 수 있고 이탈 영향을 평가할 수 있습니다.
 -- MAGIC 
--- MAGIC However, knowing that we have churn isn't enough. We now need to take it to the next level and build a predictive model to determine our customers at risk of churn and increase our revenue.
+-- MAGIC 그러나 이탈이 있다는 사실을 아는 것만으로는 충분하지 않습니다. 이제 이를 한 단계 더 발전시켜 이탈 위험이 있는 고객을 파악하고 수익을 늘리기 위한 **예측 모델** 을 구축해야 합니다.
 -- MAGIC 
--- MAGIC This is where the Lakehouse value comes in. Within the same platform, anyone can start building ML model to run such analysis, including low code solution with AutoML.
+-- MAGIC 여기에서 Lakehouse의 가치가 나타납니다. 동일한 플랫폼 내에서 누구나 **AutoML**을 사용한 로우 코드 솔루션을 포함하여 이러한 분석을 실행하기 위해 ML 모델 구축을 시작할 수 있습니다.
 
 -- COMMAND ----------
 
@@ -163,24 +162,24 @@
 
 -- MAGIC %md-sandbox
 -- MAGIC 
--- MAGIC ## Automate action to reduce churn based on predictions
+-- MAGIC ## 예측을 기반으로 이탈을 줄이기 위한 작업 자동화
 -- MAGIC 
 -- MAGIC 
--- MAGIC <img style="float: right" width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-dbsql-prediction-dashboard.png">
+-- MAGIC <img style="float: right;margin-left: 10px" width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-dbsql-prediction-dashboard.png">
 -- MAGIC 
--- MAGIC We now have an end 2 end data pipeline analizing and predicting churn. We can now easily trigger actions to reduce the churn based on our business:
+-- MAGIC 이제 고객 이탈을 분석하고 예측하는 엔드 투 엔드 데이터 파이프라인이 있습니다. 이제 비즈니스를 기반으로 이탈을 줄이기 위한 조치를 쉽게 트리거할 수 있습니다.
 -- MAGIC 
--- MAGIC - Send targeting email campaign to the customer the most likely to churn
--- MAGIC - Phone campaign to discuss with our customers and understand what's going
--- MAGIC - Understand what's wrong with our line of product and fixing it
+-- MAGIC - 이탈 가능성이 가장 높은 고객에게 타겟팅 이메일 캠페인 발송
+-- MAGIC - 고객과 논의하고 진행 상황을 이해하기 위한 전화 캠페인
+-- MAGIC - 당사 제품 라인의 문제점 파악 및 수정
 -- MAGIC 
--- MAGIC These actions are out of the scope of this demo and simply leverage the Churn prediction field from our ML model.
+-- MAGIC 이러한 작업은 이 데모의 범위를 벗어나며 단순히 ML 모델의 Churn 예측 필드를 활용합니다.
 -- MAGIC 
--- MAGIC ## Track churn impact over the next month and campaign impact
+-- MAGIC ## 다음 달 이탈 영향 및 캠페인 영향 추적
 -- MAGIC 
--- MAGIC Of course, this churn prediction can be re-used in our dashboard to analyse future churn and measure churn reduction. 
+-- MAGIC 물론 이 이탈 예측은 대시보드에서 재사용하여 향후 이탈을 분석하고 이탈 감소를 측정할 수 있습니다.
 -- MAGIC 
--- MAGIC The pipeline created with the Lakehouse will offer a strong ROI: it took us a few hours to setup this pipeline end 2 end and we have potential gain for $129,914 / month!
+-- MAGIC Lakehouse로 생성된 파이프라인은 강력한 ROI를 제공할 것입니다. 이 파이프라인 엔드 투 엔드를 설정하는 데 몇 시간이 걸렸고 월 $129,914의 잠재적인 이익을 얻었습니다!
 
 -- COMMAND ----------
 
@@ -191,14 +190,14 @@
 -- COMMAND ----------
 
 -- MAGIC %md-sandbox
--- MAGIC ## 5/ Deploying and orchestrating the full workflow
+-- MAGIC ## 5/ 전체 워크플로 배포 및 오케스트레이션
 -- MAGIC 
 -- MAGIC <img style="float: left; margin-right: 10px" width="400px" src="https://raw.githubusercontent.com/QuentinAmbard/databricks-demo/main/retail/resources/images/lakehouse-retail/lakehouse-retail-churn-5.png" />
 -- MAGIC 
 -- MAGIC <br><br><br>
--- MAGIC While our data pipeline is almost completed, we're missing one last step: orchestrating the full workflow in production.
+-- MAGIC 데이터 파이프라인이 거의 완성되었지만 프로덕션에서 전체 워크플로를 오케스트레이션 하는 마지막 단계가 누락되었습니다.
 -- MAGIC 
--- MAGIC With Databricks Lakehouse, no need to manage an external orchestrator to run your job. Databricks Workflows simplifies all your jobs, with advanced alerting, monitoring, branching options etc.
+-- MAGIC Databricks Lakehouse를 사용하면 작업을 실행하기 위해 외부 오케스트레이터를 관리할 필요가 없습니다. **Databricks Workflows**는 고급 경고, 모니터링, 분기 옵션 등을 통해 모든 작업을 단순화합니다.
 
 -- COMMAND ----------
 
@@ -209,15 +208,19 @@
 
 -- MAGIC %md
 -- MAGIC 
--- MAGIC ## Conclusion
+-- MAGIC ## 결론
 -- MAGIC 
--- MAGIC We demonstrated how to implement an end 2 end pipeline with the Lakehouse, using a single, unified and secured platform:
+-- MAGIC 우리는 통합되고 안전한 단일 플랫폼을 사용하여 Lakehouse로 엔드 투 엔드 파이프라인을 구현하는 방법을 시연했습니다.
 -- MAGIC 
--- MAGIC - Data ingestion
--- MAGIC - Data analysis / DW / BI 
--- MAGIC - Data science / ML
--- MAGIC - Workflow & orchestration
+-- MAGIC - 데이터 수집
+-- MAGIC - 데이터 분석 / DW / BI
+-- MAGIC - 데이터 사이언스/ML
+-- MAGIC - 워크플로우 및 오케스트레이션
 -- MAGIC 
--- MAGIC As result, our analyst team was able to simply build a system to not only understand but also forecast future churn and take action accordingly.
+-- MAGIC 그 결과 분석가 팀은 단순히 시스템을 구축하여 향후 변동을 이해하고 예측하고 그에 따라 조치를 취할 수 있었습니다.
 -- MAGIC 
--- MAGIC This was only an introduction to the Databricks Platform. For more details, contact your account team and explore more demos with `dbdemos.list()`
+-- MAGIC 이것은 Databricks 플랫폼에 대한 소개일 뿐입니다. 보더 자세한 내용은 영업팀에 문의하고 `dbdemos.list()`로 더 많은 데모를 살펴보십시오.
+
+-- COMMAND ----------
+
+
